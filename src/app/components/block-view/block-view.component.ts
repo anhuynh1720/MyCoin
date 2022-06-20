0c40ccb226c2964991bd9e56826cd42fdf4423b1
An <hnan18@vp.fitus.edu.vn>
2022-06-21 02:41:33 +0700

    Add transactions view when click on each block

--- src/app/components/block-view/block-view.component.ts
+++ src/app/components/block-view/block-view.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class BlockViewComponent implements OnInit {
   @Input() public block;
+  @Input() public selectedBlock;
 
   constructor() { }
 
