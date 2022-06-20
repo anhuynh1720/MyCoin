8ff9302a5e5c3813916c4cf80c5296acb6a79d1f
An <hnan18@vp.fitus.edu.vn>
2022-06-20 15:27:08 +0700

    Add blockchain viewer and update blockchain services

--- src/app/services/blockchain.service.ts
+++ src/app/services/blockchain.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+// @ts-ignore
+import { Blockchain } from 'simple-blockchain-implement';
+import { EC } from 'elliptic';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BlockchainService {
 
-  constructor() { }
+  public blockchainInstance = new Blockchain();
+
+  public walletKeys = [] as any;
+
+  constructor() {
+    this.blockchainInstance.difficulty = 1;
+    this.blockchainInstance.minePendingTransactions('my-wallet-address');
+    this.generateWalletKeys();
+  }
+
+  getBlock() {
+    return this.blockchainInstance.chain;
+  }
+
+  private generateWalletKeys() {
+    const ec = new EC.ec('secp256k1');
+    const key = ec.genKeyPair();
+
+    this.walletKeys.push({
+      keyObj: key,
+      publicKey: key.getPublic('hex'),
+      privateKey: key.getPrivate('hex'),
+    });
+
+
+  }
 }
