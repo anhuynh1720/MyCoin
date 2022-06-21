151c89fa594216f94d7c0625ed01b8980ba75f28
An <hnan18@vp.fitus.edu.vn>
2022-06-21 22:17:59 +0700

    Add create transaction component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { BlockchainViewerComponent } from './pages/blockchain-viewer/blockchain-viewer.component';
 import { BlockViewComponent } from './components/block-view/block-view.component';
 import { TransactionsTableComponent } from './components/transactions-table/transactions-table.component';
+import { CreateTransactionComponent } from './pages/create-transaction/create-transaction.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BlockchainViewerComponent,
     BlockViewComponent,
-    TransactionsTableComponent
+    TransactionsTableComponent,
+    CreateTransactionComponent
   ],
   imports: [
     BrowserModule,
